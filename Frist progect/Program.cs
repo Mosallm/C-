6a8ea55dd35325a7027a2a54f2ce709899720c5b
Hamdy <hmmslm84@gmail.com>
2022-07-06 11:14:08 +0200

    Add C# file

---
+++ Frist progect/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+        Console.WriteLine("Enter your Name : ");
+        string Name = Console.ReadLine();
+        Console.WriteLine("Welcome " + Name);
+        Console.ReadKey(true);
+        }
+    }
+
